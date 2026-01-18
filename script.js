e02fb62d43432ca468b47d6ca1b2655e670bf510
Akash Gupta <124385823+Gupta-G-0406@users.noreply.github.com>
2026-01-18 21:28:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+let currentAudio = null;
+
+document.querySelectorAll(".btn").forEach(button => {
+  button.addEventListener("click", () => {
+    const sound = button.innerText;
+
+    if (sound === "stop") {
+      if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+      }
+      return;
+    }
+
+    if (currentAudio) {
+      currentAudio.pause();
+      currentAudio.currentTime = 0;
+    }
+
+    currentAudio = new Audio(`sounds/${sound}.mp3`);
+    currentAudio.play();
+  });
+});
