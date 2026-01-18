185b81fdea977f9d48b64bdd99ba38e9634518d6
Akash Gupta <124385823+Gupta-G-0406@users.noreply.github.com>
2026-01-18 21:35:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
-const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
 let currentAudio = null;
 
 document.querySelectorAll(".btn").forEach(button => {
   button.addEventListener("click", () => {
     const sound = button.innerText;
 
+    // Stop audio
     if (sound === "stop") {
       if (currentAudio) {
         currentAudio.pause();
         currentAudio.currentTime = 0;
+        currentAudio.remove();
+        currentAudio = null;
       }
       return;
     }
 
+    // Stop previous audio
     if (currentAudio) {
       currentAudio.pause();
       currentAudio.currentTime = 0;
+      currentAudio.remove();
     }
 
-    currentAudio = new Audio(`sounds/${sound}.mp3`);
-    currentAudio.play();
+    // Create audio element (IMPORTANT)
+    const audio = document.createElement("audio");
+    audio.src = `sounds/${sound}.mp3`;
+    audio.autoplay = true;
+
+    // Append to DOM so Cypress can detect it
+    document.body.appendChild(audio);
+
+    currentAudio = audio;
   });
 });
